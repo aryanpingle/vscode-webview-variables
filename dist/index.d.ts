e0e20d7e7a6f9a1b7b112b43b8bba9fb6ab488e3
Aryan Pingle <aryan.pingle@sprinklr.com>
2024-06-25 17:28:47 +0530

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare const VscodeCSSVariables: Record<string, string>;
+export declare function isCSSVariable(str: string): boolean;
+export declare function createCSSVariable(variableName: string, ...fallbacks: string[]): string;
---
+++ test/test.js
@@ -0,0 +1,56 @@
+const {
+  createCSSVariable,
+  isCSSVariable,
+} = require("../dist/index");
+
+const Colors = {
+  GREEN: "\x1b[32m",
+  YELLOW: "\x1b[33m",
+  RED: "\x1b[31m",
+  RESET: "\x1b[0m",
+  BOLD: "\x1b[1m",
+};
+
+/**
+ * @param {any} actual
+ * @param {any} expected
+ * @param {string} testName
+ */
+function test(actual, expected) {
+  if (actual === expected) {
+    // Test passed
+    console.log(Colors.GREEN, "✅ Test passed", Colors.RESET);
+  } else {
+    // Test failed
+    console.log(Colors.RED, `❌ Test failed`, Colors.RESET);
+    console.log(
+      Colors.RED,
+      `Expected: '${expected}' / Actual: '${actual}'`,
+      Colors.RESET
+    );
+  }
+}
+
+test(createCSSVariable("--var-1"), "var(--var-1)");
+test(createCSSVariable("--var-1", "red"), "var(--var-1, red)");
+test(createCSSVariable("--var-1", "--var-2"), "var(--var-1, var(--var-2))");
+test(
+  createCSSVariable("--var-1", "--var-2", "red"),
+  "var(--var-1, var(--var-2, red))"
+);
+test(
+  createCSSVariable("--var-1", "red", "--var-2"),
+  "var(--var-1, red)"
+);
+test(
+  createCSSVariable("--var-1", undefined, "red", "--var-2"),
+  "var(--var-1, undefined)"
+);
+
+test(isCSSVariable(""), false);
+test(isCSSVariable("--valid-variable"), true);
+test(isCSSVariable("invalid"), false);
+test(isCSSVariable("-invalid variable"), false);
+test(isCSSVariable("--invalid variable"), false);
+test(isCSSVariable("--unicode-Ψ"), true);
+test(isCSSVariable("--emoji-🥶"), true);
